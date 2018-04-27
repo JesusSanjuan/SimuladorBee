d9c52713974bdada55c0c2fc8e5ddfc9e4be76e0
Lety Damián <l.damianramirez@ugto.mx>
2018-04-27 14:17:01 -0500

    Generacion de los valores de X para TMAR y Y para VPN

--- WebSite/User/Vpn.aspx.cs
+++ WebSite/User/Vpn.aspx.cs
@@ -16,33 +16,61 @@ public partial class User_Default : System.Web.UI.Page
       //  Inversion.Attributes.Add("Type", "number");
     }
 
-    void GreetingBtn_Click(Object sender, EventArgs e)
+    double CalcularVPN(double fTMAR)
     {
-        
         double P = Convert.ToSingle(Inversion.Text);
         double fFNE = Convert.ToSingle(FNE.Text);
         double fVS = Convert.ToSingle(VdS.Text);
-        double fTMAR = Convert.ToDouble(TMAR.Text);
         double FNEAcumulado, fVPN;
-        int Periodo= Convert.ToInt32(n.Text);
-        int  j;
+        int Periodo = Convert.ToInt32(n.Text);
+        int i;
 
         FNEAcumulado = 0;
-        double DivTMAR= 1 + fTMAR/100;
-        for (j = 1; j < Periodo; j++)
+        double DivTMAR = 1 + fTMAR;
+        for (i = 1; i < Periodo; i++)
         {
-            FNEAcumulado = FNEAcumulado + (fFNE / Math.Pow(DivTMAR, j));
+            FNEAcumulado = FNEAcumulado + (fFNE / Math.Pow(DivTMAR, i));
         }
-        FNEAcumulado = FNEAcumulado + ((fFNE + fVS) / Math.Pow(DivTMAR, j));
+        FNEAcumulado = FNEAcumulado + ((fFNE + fVS) / Math.Pow(DivTMAR, i));
         fVPN = FNEAcumulado - P;
-        VPN.Text = Convert.ToString(fVPN);
+        
+        return fVPN;
+    }
+
+    void GreetingBtn_Click(Object sender, EventArgs e)
+    {
+        double ResultadoVPN;
+        double fTMAR = Convert.ToSingle(TMAR.Text);
+        int Periodo= Convert.ToInt32(n.Text);
+        int i;
+        double[] X = new double[1000];
+        double[] Y = new double[1000];
+
+        ResultadoVPN = CalcularVPN(fTMAR/100);
+        VPN.Text = Convert.ToString(ResultadoVPN);
+
+        /*
+        Codigo para generar arreglos con valores para X y Y
+        X guarda TMAR de 0 a 1 en incrementos de 0.001
+        Y guarda los valores de VPN 
+        */
+        fTMAR = 0;
+
+        for (i = 0; i < 1000; i++)
+        {
+            X[i] = fTMAR;
+            Y[i] = CalcularVPN(fTMAR);
+            fTMAR += 0.001;
+        }
+
+        /***************************************************************/
+
         Console.WriteLine("You click me ...................");
         theDiv.Visible = true;
-        
+       
         //Button clickedButton = (Button)sender;
         //  clickedButton.Text = "...button clicked...";
         // clickedButton.Enabled = false;
-
     }
 
 

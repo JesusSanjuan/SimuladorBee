a598caa64115e2eb7c3036b4e7ebda329842cf23
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2018-06-15 11:15:25 -0500

    Correcion

--- WebSite/Scripts/startbootstrap/js/sb-admin-datatables-van.js
+++ WebSite/Scripts/startbootstrap/js/sb-admin-datatables-van.js
@@ -1,29 +1,55 @@
 
-$(document).ready(function () {
+var ctx = document.getElementById("dataTableVAN");
+
+
+$(document).ready(function ($) {
+
+
+    var data = [
+        [
+            "Tiger Nixon",
+            "System Architect",
+            "Edinburgh",
+            "5421",
+            "2011/04/25",
+            "$3,120"
+        ],
+        [
+            "Garrett Winters",
+            "Director",
+            "Edinburgh",
+            "8422",
+            "2011/07/25",
+            "$5,300"
+        ]
+    ]
+
     var table = $('#dataTableVAN').DataTable({
-        language: {
-            processing: "Obteniendo resultados...",
-            search: "Buscar en la tabla:",
-            lengthMenu: "Ver _MENU_ resultados",
-            info: "Visualizacion de los resultados: _START_ al _END_ de un total de _TOTAL_ resultados",
-            infoEmpty: "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
-            infoFiltered: "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
-            infoPostFix: "",
-            loadingRecords: "Cargando...",
-            zeroRecords: "Cero registros",
-            emptyTable: "Ultimo",
-            paginate: {
-                first: "Primera",
-                previous: "Pagina Anterior",
-                next: "Pagina Siguiente",
-                last: "Anterior"
-            },
-            aria: {
-                sortAscending: ": Activar para ordenar la columna en orden ascendente",
-                sortDescending: ": Activar para ordenar la columna en orden descendente"
-            }
-        },
-    });
+                language: {
+                    processing: "Obteniendo resultados...",
+                    search: "Buscar en la tabla:",
+                    lengthMenu: "Ver _MENU_ resultados",
+                    info: "Visualizacion de los resultados: _START_ al _END_ de un total de _TOTAL_ resultados",
+                    infoEmpty: "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+                    infoFiltered: "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+                    infoPostFix: "",
+                    loadingRecords: "Cargando...",
+                    zeroRecords: "Cero registros",
+                    emptyTable: "Ultimo",
+                    paginate: {
+                        first: "Primera",
+                        previous: "Pagina Anterior",
+                        next: "Pagina Siguiente",
+                        last: "Anterior"
+                    },
+                    aria: {
+                        sortAscending: ": Activar para ordenar la columna en orden ascendente",
+                        sortDescending: ": Activar para ordenar la columna en orden descendente"
+                    }
+                    },
+                    data: data
+            });
+    
 
 });
     

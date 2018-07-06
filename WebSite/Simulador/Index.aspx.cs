d8f8ef189ca1a64cfe42a3ff616f5b4e4d2be0a5
ROSALBA PACHECO SANCHEZ <r.pacheco.sanchez@ugto.mx>
2018-07-06 09:14:24 -0500

    avances en el guardado de proyecto

--- WebSite/Simulador/Index.aspx.cs
+++ WebSite/Simulador/Index.aspx.cs
@@ -4,22 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
 
 public partial class User_Index : System.Web.UI.Page
 {
     protected void Guardar_ProyectoBtn_Click(Object sender, EventArgs e)
     {
+        string nombre_proyecto = Nombre_Proyecto.Text;
+        string id_periodo      = nperiodo.Value.Substring(0, 1) + lapso.Value.ToString().Substring(0, 1);
+        string id_user         = User.Identity.GetUserId();/*para obtener el id de usuario utilizando icrosoft.AspNet.Identity */
+
         // Probando a guardar datos de ejrmplo en la BD
-        /*var db = new Entidades();
-        var NuevoProyecto = new Proyecto();
-        NuevoProyecto.ID_Proyecto = "0001";
-        NuevoProyecto.ID_Usuario = "f68f0294-060b-436e-91e0-098ca8436170";
-        NuevoProyecto.Nombre_Proyecto = Nombre_Proyecto.Text;
-        NuevoProyecto.Fecha_Hora = DateTime.Now;
-        NuevoProyecto.ID_Periodo = "0";
+        var db = new Entidades();
+        var NuevoProyecto           = new Proyecto();
+        NuevoProyecto.ID_Proyecto   = "0001";
+        NuevoProyecto.ID_Usuario    = id_user;
+        NuevoProyecto.Nombre_Proyecto = nombre_proyecto;
+        NuevoProyecto.Fecha_Hora    = DateTime.Now;
+        NuevoProyecto.ID_Periodo    = id_periodo;
         db.Proyecto.Add(NuevoProyecto);
-        db.SaveChanges();*/
-        System.Diagnostics.Debug.WriteLine(Nombre_Proyecto.Text);
+        db.SaveChanges();
+        System.Diagnostics.Debug.WriteLine(nombre_proyecto + "--"+ id_periodo +"--"+ id_user);
     }
 
         protected void Page_Load(object sender, EventArgs e)
